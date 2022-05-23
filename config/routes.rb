71c00c7fd9b2a6664dea0da84dad1bf8168949c0
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-05-23 21:26:34 +0000

    [Add]管理者ログイン画面を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # 管理者用
+  # URL/admin/sign_in...
+  # devise_forの後に生成したコントローラーがどこに存在するかを記述
+  # skipオプション：不要なルーティングであるパスワード変更・管理者側の登録を削除
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
 end
