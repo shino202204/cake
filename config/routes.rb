e7cc569dd752293a7afbba4b9a0fa508013918f6
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-05-30 14:17:20 +0000

    [Add]Customerモデル・コントローラを追加

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,10 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 end
