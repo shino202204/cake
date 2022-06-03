b020cd5fff61c3ad5f50ceee428589c9e8285321
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-06-03 10:49:46 +0000

    [Add]商品のカート追加機能を実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,8 +1,19 @@
 class Public::CartItemsController < ApplicationController
   def index
   end
-  
+
   def create
-    render :index
+    @cart_item = CartItem.new(cart_item_params)
+    if @cart_item.save
+      flash[:notice] = "CartItem was successfully created."
+      redirect_to cart_items_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
   end
 end
