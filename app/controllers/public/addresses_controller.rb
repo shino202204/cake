31c15bdad00a26d69599f1821ac2ff1e0b0bef3a
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-06-07 21:25:16 +0000

    [Add]配送先登録/一覧機能を追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,8 +1,28 @@
 class Public::AddressesController < ApplicationController
   def index
     puts "indexアクションへ到達しました"
+    @address = Address.new
+    @addresses = Address.all
+  end
+
+  def create
+    address = Address.new(address_params)
+    @addresses = Address.all
+    if address.save
+      flash[:notice] = "Address was successfully created."
+      redirect_to addresses_path
+    else
+      render :index
+    end
   end
 
   def edit
+    puts "editアクションへ到達しました"
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name, :customer_id)
   end
 end
