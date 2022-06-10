a1e900dae2a15d79dea4754169ddc0b7b0a31cc9
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-06-10 13:15:25 +0000

    [Add]注文情報確認画面を更新

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,6 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
     @main_address = current_customer.address
     # @main_address = current_customer.address.address_display
     # @main_address = Address.find_by(customer_id: current_customer.id)
@@ -7,6 +8,10 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     puts 'confirmアクションに到達しました'
+    # ユーザーのカートアイテム情報
+    @cart_items = current_customer.cart_items.all
+    @total_payment = 0
+    @shipping_cost = 800
   end
 
   def complete
