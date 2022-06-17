df76d5cc71f3d15e5813e5b43b6461ce84b17338
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-06-17 10:45:03 +0000

    [Add]注文ステータスenum/statusを追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
   # 支払方法 0:クレジットカード, 1:銀行振込
   enum payment_method: {credit_card: 0, transfer: 1}
+  # 注文ステータス 0:入金待ち, 1:入金, 2:製作中, 3:発送準備中, 4:発送済み
+  enum status: {awaiting_payment: 0, payment_confirmation: 1, under_manufacture: 2, preparing_to_ship: 3, shipped: 4}
 
   # アソシエーション
   belongs_to :customer
