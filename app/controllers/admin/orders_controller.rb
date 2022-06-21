95116701ad8505f4be3068287115afed514b1b57
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-06-21 21:44:05 +0000

    [Update]注文ステータスのupdateアクションを更新

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,6 +5,16 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
+    puts "注文ステータスのupdateアクションです"
+    puts "受け取ったパラメータは#{params[:order][:status]}です"
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
+    render :show
+  end
 
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
   end
 end
