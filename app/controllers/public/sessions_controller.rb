c65b4cea38da6c1a415dc100eeacc41332e2e841
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-06-22 03:29:44 +0000

    [Add]ログイン時の退会判断メソッドを追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,28 @@
 
 class Public::SessionsController < Devise::SessionsController
 
+  before_action :customer_state, only: [:create]
+
+  protected
+  # 退会しているかを判断するメソッド
+  def customer_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @customer.valid_password?(params[:customer][:password])
+      ## 【処理内容3】
+      if @customer.is_deleted == false
+      end
+
+      if @customer.is_deleted == true
+        redirect_to new_customer_session
+      end
+    end
+  end
+
+
   # ログイン後の遷移先
   def after_sign_in_path_for(resource)
     root_path
