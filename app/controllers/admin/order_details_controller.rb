3b021bd2d50742c0a592db65cce110856311f6f7
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-06-23 03:31:08 +0000

    [Update]order_details#updateを更新

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,8 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
+    puts "order_detailsのupdateアクションです"
+    # render :admin_order_path
+    redirect_to admin_order_path
+    # redirect_to root_path
   end
 end
