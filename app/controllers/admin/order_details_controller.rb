6170888bf51ea6c60252932d3eea227f5fef2c03
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-06-24 03:32:22 +0000

    [Add]order_detail#updateに確認用の記述を追加

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,8 +1,21 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
     puts "order_detailsのupdateアクションです"
-    # render :admin_order_path
+
+    # @order = Order.find(params[:id])
+    # @order_details = OrderDetail.where(order_id: @order.id)
+    # render 'admin/orders/show'
+
+    # パラメータ取得
+    puts "受け取った製作パラメータは#{params[:order_detail][:making_status]}です"
+
     redirect_to admin_order_path
     # redirect_to root_path
   end
+
+  # private
+
+  # def order_detail_params
+  #   params.require(:order_detail).permit(:making_status)
+  # end
 end
