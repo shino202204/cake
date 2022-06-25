8023c133c2b6eab1f5f067fbf10e1d07de754290
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-06-25 03:00:59 +0000

    [Update]注文ステータス機能を更新

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,16 +1,38 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
+    # 確認用
     puts "order_detailsのupdateアクションです"
-
-    # @order = Order.find(params[:id])
-    # @order_details = OrderDetail.where(order_id: @order.id)
-    # render 'admin/orders/show'
-
-    # パラメータ取得
     puts "受け取った製作パラメータは#{params[:order_detail][:making_status]}です"
+    puts "受け取ったorder_detailのidは#{params[:id]}です"
 
-    redirect_to admin_order_path
-    # redirect_to root_path
+    # Orderのidを取得
+    @order_id = OrderDetail.find(params[:id]).order_id
+    puts "注文idは#{@order_id}です"
+
+    # 注文と注文詳細を取得
+    @order = Order.find(@order_id)
+    @order_detail = OrderDetail.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
+
+    # 製作ステータスを更新
+    @making_status = params[:order_detail][:making_status]
+    puts "製作ステータスは#{@making_status}です"
+
+    # 製作中
+    if @making_status.to_i == 2
+      @order_detail.update(making_status: 2)
+    end
+    # 製作完了
+    if @making_status.to_i == 3
+      @order_detail.update(making_status: 3)
+    end
+    # 製作ステータスが全て「製作完了」の場合、
+    # 注文ステータスを「発送準備中」にする。
+    
+
+
+    # 注文履歴詳細に遷移
+    render 'admin/orders/show'
   end
 
   # private
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,17 +7,20 @@ class Admin::OrdersController < ApplicationController
   def update
     puts "注文ステータスのupdateアクションです"
     puts "受け取った受注パラメータは#{params[:order][:status]}です"
-    puts "受け取った製作パラメータは#{params[:order_detail][:making_status]}です"
+
     # パラメータを取得
     @status = params[:order][:status]
+    puts "製作パラメータは#{@status}です"
     # 注文(Order)を取得
     @order = Order.find(params[:id])
     # 注文詳細（OrderDetail）を取得
     @order_details = OrderDetail.where(order_id: @order.id)
 
     # 注文の受注ステータスが入金確認（1）の場合
-    if @status == 1
+    if @status.to_i == 1
+      puts "ifブロックに入りました"
       @order.update(status: 1)
+      puts"注文ステータスをupdateしました"
       # 紐づく注文詳細の製作ステータスを製作待ち（1）へ更新
       @order_details.each do |order_detail|
         order_detail.update(making_status: 1)
