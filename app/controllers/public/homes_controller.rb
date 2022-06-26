5a1c00bbc5314f1c3fa3fff9b05cd70ec6502c05
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-06-26 12:21:04 +0000

    [Update]ECサイトTOPページのレイアウトを修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,8 @@ class Public::HomesController < ApplicationController
 
   def top
     @items = Item.all
+    #idの降順に並び替えた結果の４件を取得
+    @items = @items.order(id: :DESC).limit(4)
   end
 
   def about
